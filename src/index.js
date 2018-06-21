c5c321da070e659446e6a8f8cd5d78a8f3f880fa
Matt Travi <matt@gaincompliance.com>
2018-06-21 13:36:46 -0500

    feat(response): added the response formatter
    but without latency for now

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
+import querystring from 'querystring';
+
 const withTag = (tag, data) => [data, `[${tag}]`];
+
+export function response({method, instance, path, query, statusCode, responseTime, source, httpVersion}) {
+  const queryString = querystring.stringify(query);
+  const requestUrl = `${path}${queryString ? `?${queryString}` : ''}`;
+
+  return withTag(
+    'response',
+    {
+      res: {
+        instance,
+        method,
+        path,
+        statusCode,
+        responseTime: `${responseTime}ms`,
+        ...queryString && {query: queryString}
+      },
+      httpRequest: {
+        requestMethod: method,
+        requestUrl,
+        status: statusCode,
+        referer: source.referer,
+        userAgent: source.userAgent,
+        remoteIp: source.remoteAddress,
+        protocol: httpVersion
+      }
+    }
+  ).concat([requestUrl]);
+}
 
 export function error(payload) {
   return withTag(
--- test/unit/formatters-test.js
+++ test/unit/formatters-test.js
@@ -1,12 +1,67 @@
 import {assert} from 'chai';
 import any from '@travi/any';
-import {error} from '../../src';
+import {error, response} from '../../src';
 
 suite('formatters', () => {
+  const method = any.word();
+
+  suite('response', () => {
+    const path = any.word();
+
+    test('that the response formatter includes `httpRequest` for stackdriver', () => {
+      const instance = any.word();
+      const statusCode = any.integer();
+      const responseTime = any.integer();
+      const referer = any.string();
+      const userAgent = any.string();
+      const remoteAddress = any.string();
+      const httpVersion = any.string();
+
+      assert.deepEqual(
+        response({
+          method,
+          instance,
+          path,
+          statusCode,
+          responseTime,
+          source: {referer, userAgent, remoteAddress},
+          httpVersion
+        }),
+        [
+          {
+            res: {method, instance, path, statusCode, responseTime: `${responseTime}ms`},
+            httpRequest: {
+              requestMethod: method,
+              requestUrl: path,
+              status: statusCode,
+              referer,
+              userAgent,
+              remoteIp: remoteAddress,
+              protocol: httpVersion
+            }
+          },
+          '[response]',
+          path
+        ]
+      );
+    });
+
+    test('that the query is added to the path for the `requestUrl` when present', () => {
+      const query = any.simpleObject();
+      const queryString = Object.entries(query).map(pair => pair.map(x => encodeURIComponent(x)).join('=')).join('&');
+      const fullRequestPath = `${path}?${queryString}`;
+
+      const [formatted, , message] = response({path, query, source: {}});
+
+      assert.equal(formatted.httpRequest.requestUrl, fullRequestPath);
+      assert.equal(message, fullRequestPath);
+      assert.equal(formatted.res.query, queryString);
+    });
+  });
+
   test('that the error formatter includes `httpRequest` for stackdriver', () => {
     const errorMessage = any.sentence();
     const err = {...any.simpleObject(), message: errorMessage};
-    const method = any.word();
     const url = any.url();
 
     assert.deepEqual(
